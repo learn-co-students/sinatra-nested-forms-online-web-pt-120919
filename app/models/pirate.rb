0477e148eab904bde2f0b21d2e4fc75cce1fcf6b
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-22 20:47:50 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,9 +3,9 @@ class Pirate
    PIRATES = []
    
    def initialize(name, weight, height)
-     @name = params[:name] 
-     @weight = params[:weight]
-     @height = params[:height]
+     @name = name 
+     @weight = weight
+     @height = height
      PIRATES << self 
    end 
    
