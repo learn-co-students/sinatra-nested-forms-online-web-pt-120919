4f36c1b72f2e3871651f8942874a4003fd3cd7c7
asijaw <asija.w@yahoo.com>
2020-03-25 13:58:05 -0500

    start  build of pirate and ship classes and setting under controller

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+    attr_accessor :name, :type, :booty_attribuutes
+    @all = []
+
+    def initialize()
+        @all << self
+    end
+
+    def self.all
+        @all
+    end
+
+    def self.clear
+        @all.clear
+    end 
+
 end
